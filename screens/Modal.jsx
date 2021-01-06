293b208754c300e2bc569b3ebfa848a17d88bccf
Ricardo Olarte <raop155@hotmail.com>
2021-01-06 17:18:11 -0500

    feature(Modal) Fetch data and show on Modal Component

--- screens/Modal.jsx
+++ screens/Modal.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import useFetch from '../hooks/useFetch';
 
-const Modal = () => {
+const Modal = ({ navigation }) => {
+  const _id = navigation.getParam('_id');
+  const { loading, data } = useFetch({
+    url: `https://serverless.raop155.vercel.app/api/meals/${_id}`,
+  });
+
   return (
-    <View>
-      <Text>Modal Screen</Text>
+    <View style={styles.container}>
+      {loading ? (
+        <View>
+          <ActivityIndicator size='large' color='#00f' />
+          <Text>Loading...</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>{data._id}</Text>
+          <Text>{data.name}</Text>
+          <Text>{data.description}</Text>
+        </View>
+      )}
     </View>
   );
 };
 
 export default Modal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
