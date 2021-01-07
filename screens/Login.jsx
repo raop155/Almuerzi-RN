8c1e1ce2593bf851c17edeb1f6455169cdd89fb6
Ricardo Olarte <raop155@hotmail.com>
2021-01-07 15:37:48 -0500

    hook(useForm) create useForm Custom Hook

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import useForm from '../hooks/useForm';
 
 const Login = ({ navigation }) => {
+  const initialState = {
+    email: '',
+    password: '',
+  };
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const { subscribe, inputs, handleSubmit } = useForm(initialState, onSubmit);
+
   const goRegisterScreen = () => {
     navigation.navigate('Register');
   };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-      <TextInput style={styles.input} placeholder='Email' />
-      <TextInput style={styles.input} placeholder='Password' />
-      <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={() => {}}>
+      <TextInput
+        style={styles.input}
+        onChangeText={subscribe('email')}
+        value={inputs.email}
+        placeholder='Email'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={subscribe('password')}
+        value={inputs.password}
+        placeholder='Password'
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={handleSubmit}>
         <Text style={styles.buttonText}>Log in</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={goRegisterScreen}>
