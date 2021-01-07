d9ea1f959ee4d291b9e5d605fd3d05bf8a184a9d
Ricardo Olarte <raop155@hotmail.com>
2021-01-07 13:03:39 -0500

    bugfix(useFetch) Fix memory leak

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -5,12 +5,20 @@ const useFetch = ({ url }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    let mounted = true;
+
     fetch(url)
       .then((data) => data.json())
       .then((data) => {
-        setData(data);
-        setLoading(false);
+        if (mounted) {
+          setData(data);
+          setLoading(false);
+        }
       });
+
+    return () => {
+      mounted = false;
+    };
   }, []);
 
   return { loading, data };
